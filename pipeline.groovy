b334db34e83689ddd7d1e91ea6d04f717d697cce
yangxu.lv <lvyangxu@avalongames.cn>
2022-06-07 17:03:56 +0800

    测试

--- pipeline.groovy
+++ pipeline.groovy
@@ -9,7 +9,7 @@ pipeline {
         stage('拉取统一构建脚本') {
             steps {
                 sh 'rm -rf ${WORKSPACE}'
-                sh 'git clone --depth=1 "git@git.avalongames.com:web_util/avalon_web_cd.git"'
+                // sh 'git clone --depth=1 "git@git.avalongames.com:web_util/avalon_web_cd.git"'
             }
         }
 
