5ed316743e989e40ff659b11197c130430e5494f
yangxu.lv <lvyangxu@avalongames.cn>
2022-06-07 17:07:10 +0800

    测试

--- pipeline.groovy
+++ pipeline.groovy
@@ -6,12 +6,13 @@ pipeline {
     } 
 
     stages {
-        stage('拉取统一构建脚本') {
-            steps {
-                sh 'rm -rf ${WORKSPACE}/*'
-                // sh 'git clone --depth=1 "git@git.avalongames.com:web_util/avalon_web_cd.git"'
-            }
-        }
+        // stage('拉取统一构建脚本') {
+        //     steps {
+        //         sh 'rm -rf ${WORKSPACE}'
+        //         sh 'mkdir ${WORKSPACE}'
+        //         // sh 'git clone --depth=1 "git@git.avalongames.com:web_util/avalon_web_cd.git"'
+        //     }
+        // }
 
         stage('拉取项目仓库') {
             steps {
