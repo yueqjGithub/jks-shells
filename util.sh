302ab61a7938e6e05508cf460bd50385ae6aca5d
yangxu.lv <lvyangxu@avalongames.cn>
2022-06-07 18:12:20 +0800

    测试

--- util.sh
+++ util.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # 拉取仓库代码
-function avalon_web_cd::pull_repo(){
-    repo=$1
-    branch=$2
-    svnVersion=$3
+function avalon_web_cd::pull_repo() {
+    repo="$1"
+    branch="$2"
+    svnVersion="$3"
 
     if [[ ${repo} == '' ]]; then
         echo '未设置仓库地址'
@@ -19,7 +19,7 @@ function avalon_web_cd::pull_repo(){
     if [[ ${repo} == 'git@*' ]]; then
         echo '从git拉取代码'
         git clone --depth=1 "${repo}"
-        return
+        return 1
     fi
 
     if [[ ${repo} == 'https://svn*' ]]; then
@@ -27,13 +27,13 @@ function avalon_web_cd::pull_repo(){
         #获取svn最新版本号
         if [[ $3 == 'latest' ]]; then
             for i in $(svn info "${repo}/${branch}" --trust-server-cert --non-interactive | grep Revision); do
-            svnVersion=$(echo "${i}" | sed 's:Revision\: ::g')
+                svnVersion=$(echo "${i}" | sed 's:Revision\: ::g')
             done
         fi
 
         svn co "${repo}/${branch}" -r "${svnVersion}" --trust-server-cert --non-interactive
-        return
+        return 1
     fi
 
-    exit 1
+    return 0
 }
