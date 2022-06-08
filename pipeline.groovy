80b3c987b76a2385310023daa59b5b0abbfdeebe
yangxu.lv <lvyangxu@avalongames.cn>
2022-06-08 10:24:03 +0800

    测试

--- pipeline.groovy
+++ pipeline.groovy
@@ -14,20 +14,6 @@ pipeline {
     }
 
     stages {
-
-        stage('Example') {
-            steps {
-                checkout([$class: 'GitSCM',
-                          branches: [[name: "${params.BRANCH_TAG}"]],
-                          doGenerateSubmoduleConfigurations: false,
-                          extensions: [],
-                          gitTool: 'Default',
-                          submoduleCfg: [],
-                          userRemoteConfigs: [[url: 'https://github.com/jenkinsci/git-parameter-plugin.git']]
-                        ])
-            }
-        }
-
         // stage('拉取统一构建脚本') {
         //     steps {
         //         sh 'rm -rf ${WORKSPACE}'
--- util.sh
+++ util.sh
@@ -2,8 +2,8 @@
 
 # 拉取仓库代码
 function avalon_web_cd_pull_repo() {
-    repo="$1"
-    branch="$2"
+    local repo="$1"
+    local branch="$2"
 
     if [[ ${repo} == '' ]]; then
         echo '未设置仓库地址'
@@ -21,7 +21,7 @@ function avalon_web_cd_pull_repo() {
         return 0
     elif [[ ${repo} == https://svn* ]]; then
         echo '从svn拉取代码'
-        svnVersion="$3"
+        local svnVersion="$3"
         #获取svn最新版本号
         if [[ $3 == 'latest' ]]; then
             for i in $(svn info "${repo}/${branch}" --trust-server-cert --non-interactive | grep Revision); do
