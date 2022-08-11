7add74d7352b15fb248168e51eeb853e596e30d7
yangxu.lv <lvyangxu@avalongames.cn>
2022-08-11 16:29:04 +0800

    测试

--- util.sh
+++ util.sh
@@ -53,7 +53,7 @@ function avalon_web_cd_build_app() {
     for app in ${apps[@]}; do
 
         appConfigStr=${app##*/}
-        appPath=$(bash ${WORKSPACE}/custom_string_parse.sh ${appConfigStr})
+        appPath=$(bash -x ${WORKSPACE}/custom_string_parse.sh ${appConfigStr})
         appName=$(echo "${appPath}" | sed -r 's/.+\///g')
 
         echo "开始构建应用${appName}"
@@ -74,13 +74,13 @@ function avalon_web_cd_build_app() {
         elif [[ -f 'pom.xml' ]]; then
             appType='java'
             #获取pom.xml中的包名
-            jarPath=$(bash ${WORKSPACE}/custom_string_parse.sh ${appConfigStr} jar包路径)
+            jarPath=$(bash -x ${WORKSPACE}/custom_string_parse.sh ${appConfigStr} jar包路径)
             if [[ jarPath == "" ]]; then
                 echo "未配置jar包路径"
                 exit 1
             fi
 
-            finalName=$(bash ${WORKSPACE}/pom_final_name_parse.sh ${WORKSPACE}/build/${appPath}/pom.xml)
+            finalName=$(bash -x ${WORKSPACE}/pom_final_name_parse.sh ${WORKSPACE}/build/${appPath}/pom.xml)
             buildFile="${WORKSPACE}/build/${jarPath}"
             # 如果java应用的仓库根目录就是应用，则使用jar名称作为应用名
             if [[ appName == "" ]]; then
