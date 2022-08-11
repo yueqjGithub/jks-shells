d750a24707a29cef71f1157d79e1b584717e10ee
yangxu.lv <lvyangxu@avalongames.cn>
2022-08-11 17:25:48 +0800

    测试

--- util.sh
+++ util.sh
@@ -53,7 +53,7 @@ function avalon_web_cd_build_app() {
     for app in ${apps[@]}; do
 
         appConfigStr=${app}
-        appPath=$(bash ${WORKSPACE}/custom_string_parse.sh ${appConfigStr})
+        appPath=$(bash -x ${WORKSPACE}/custom_string_parse.sh ${appConfigStr})
         appName=$(echo "${appPath}" | sed -r 's/.+\///g')
 
         echo "开始构建应用${appName}"
@@ -73,8 +73,7 @@ function avalon_web_cd_build_app() {
             appType='node'
         elif [[ -f 'pom.xml' ]]; then
             appType='java'
-            #获取pom.xml中的包名
-            jarPath=$(bash ${WORKSPACE}/custom_string_parse.sh ${appConfigStr} jar包路径)
+            jarPath=$(bash -x ${WORKSPACE}/custom_string_parse.sh ${appConfigStr} jar包路径)
             if [[ jarPath == "" ]]; then
                 echo "未配置jar包路径"
                 exit 1
@@ -83,7 +82,7 @@ function avalon_web_cd_build_app() {
             buildFile="${WORKSPACE}/build/${jarPath}"
             # 如果java应用的仓库根目录就是应用，则使用jar名称作为应用名
             if [[ ${appName} == "" ]]; then
-                deployAppName=$(bash ${WORKSPACE}/custom_string_parse.sh ${appConfigStr} 部署名称)
+                deployAppName=$(bash -x ${WORKSPACE}/custom_string_parse.sh ${appConfigStr} 部署名称)
                 if [[ ${deployAppName} == "" ]]; then
                     echo "未配置部署名称"
                     exit 1
