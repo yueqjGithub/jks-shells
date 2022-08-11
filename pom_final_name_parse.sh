cc70e40623cc9b99d7462f84d305d3344f21c23b
yangxu.lv <lvyangxu@avalongames.cn>
2022-08-11 15:29:02 +0800

    测试

--- pom_final_name_parse.sh
+++ pom_final_name_parse.sh
@@ -10,7 +10,7 @@ fi
 str=$(cat "${pomPath}")
 
 matchResult=$(echo "${str}" | sed -rn 's/^.*<finalName>\s*([^<>]+)\s*<\/finalName>.*$/\1/p')
-echo "$matchResult"
+
 if [[ ${#matchResult[*]} == 1 ]]; then
     echo matchResult[0]
 else
