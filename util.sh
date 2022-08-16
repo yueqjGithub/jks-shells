4d22e3b5a6bfaaf45042e2383f1c353b7f2e7e65
yangxu.lv <lvyangxu@avalongames.cn>
2022-08-16 17:51:54 +0800

    测试

--- util.sh
+++ util.sh
@@ -266,17 +266,11 @@ echo "#解压并移动到指定目录"
 # 创建用于更新的临时目录
 rm -rf ${deployDir}/update_tmp
 mkdir ${deployDir}/update_tmp
-cp /tmp/${zipname} ${deployDir}/update_tmp/ || exit 1
-if [[ -f ${deployDir}/update_tmp/${zipname} ]]; then
-    echo "zip更新包:${deployDir}/update_tmp/${zipname}"
-else
-    echo "zip更新包不存在:${deployDir}/update_tmp/${zipname}"
-    exit 1
-fi
+mv -f /tmp/${zipname} ${deployDir}/update_tmp/ || exit 1
 cd ${deployDir}/update_tmp || exit 1
 # 查看目录结构，获取要更新的应用列表
 zipStruct=\$(unzip -l "${zipname}" | sed -rn "s/^\s+[0-9]+\s+[0-9:]+.+\s+(\S+)$/\1/p")
-updateApps=\$(echo \${zipStruct} | sed -rn "s/^([^/]+\/)$/\1/p" | sed -rn "s/^([^/]+)\/*$/\1/p")
+updateApps=\$(echo "\${zipStruct}" | sed -rn "s/^([^/]+\/)$/\1/p" | sed -rn "s/^([^/]+)\/*$/\1/p")
 echo "更新的应用列表:${updateApps}"
 
 # appZips=$(ls *.zip 2> /dev/null | wc -l)
