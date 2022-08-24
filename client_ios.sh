577ab44c760475ef7367b6cd67925b9c21bc31d5
quanjiang.yue <yuequanjiang@avalongames.cn>
2022-08-24 18:16:14 +0800

    test

--- client_ios.sh
+++ client_ios.sh
@@ -1,7 +1,7 @@
 echo 'ready to build ios client'
 
 pwd
-# 执行autoBuild.sh
+# 执行autoBuild.sh~
 cd ${WORKSPACE}/ios_avalon/AvalonUIKit || exit 1
 sh autoBuild.sh || exit 1
 cd ${WORKSPACE}/ios_avalon/AvalonFoundation || exit 1
