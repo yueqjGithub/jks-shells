cf8b0023bb5abe61f5863da52a851f988e817435
quanjiang.yue <yuequanjiang@avalongames.cn>
2022-08-25 13:48:57 +0800

    fix

--- client_ios.sh
+++ client_ios.sh
@@ -17,7 +17,7 @@ fi
 
 mv ${WORKSPACE}/ios_avalon/AvalonUIKit/AProducts/AvalonUIKit.xcframework/ios-arm64_armv7/AvalonUIKit.framework ${WORKSPACE}/dist/
 
-# 压缩成果包
+echo "压缩成果包"
 zipName=${JOB_BASE_NAME}_${appVersion}_R${GIT_COMMIT:0:6}_B${BUILD_NUMBER}
 txtName=${zipName}.txt
 
@@ -29,8 +29,8 @@ mv `ls | grep -v client_ios` client_ios/
 
 zip -r -q "${zipName}" client_ios/
 
-if [[ ${versioncode_w} == null ]]; then
-    echo "未定义versioncode_w，使用默认值release"
-fi
+#if [[ ${versioncode_w} == null ]]; then
+#    echo "未定义versioncode_w，使用默认值release"
+#fi
 
-md5sum "${zipName}.zip" | cut -d ' ' -f1 | tee "${txtName}"
+#md5sum "${zipName}.zip" | cut -d ' ' -f1 | tee "${txtName}"
