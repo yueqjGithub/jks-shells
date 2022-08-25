d3f1fa7fb5aec964733ac9c782ea4a6e97a1a89a
quanjiang.yue <yuequanjiang@avalongames.cn>
2022-08-25 11:50:36 +0800

    复制成品到dist

--- client_ios.sh
+++ client_ios.sh
@@ -6,3 +6,13 @@ cd ${WORKSPACE}/ios_avalon/AvalonUIKit || exit 1
 sh autoBuild.sh || exit 1
 cd ${WORKSPACE}/ios_avalon/AvalonFoundation || exit 1
 sh autoBuild.sh || exit 1
+
+# 复制成果文件
+cd ${WORKSPACE}
+if [ -d 'dist' ]; then
+    rm -rf dist
+else
+    mkdir dist
+fi
+
+cp ${WORKSPACE}/ios_avalon/AvalonUIKit/AProducts/AvalonUIKit.xcframework/ios-arm64_armv7/AvalonUIKit.framework ${WORKSPACE}/dist
