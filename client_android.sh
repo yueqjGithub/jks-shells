9c2efdb8263452dd12c3f6c9773900b6abeba9c9
quanjiang.yue <yuequanjiang@avalongames.cn>
2022-09-22 14:09:11 +0800

    fix

--- client_android.sh
+++ client_android.sh
@@ -37,7 +37,6 @@ sed -i "1i boolean library=${libraryVal}" AvalonGameCenter/build.gradle | exit 1
 echo "修改library参数完毕"
 
 echo '将/AvalonSSDKFramework/src/main/assets/avalon_supersdk_properties.json中super_sdk_version的值改为appVersion'
-sed -i '/super_sdk_version/ d' AvalonSSDKFramework/src/main/assets/avalon_supersdk_properties.json | exit 1
 version_line_num=$(echo $(sed -n '/\/super_sdk_version/=' /AvalonSSDKFramework/src/main/assets/avalon_supersdk_properties.json))
 version_line_num=$((10#${version_line_num}-1))
 echo "${version_line_num}"
