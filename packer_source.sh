5fd0fc88e59c87ecd4cc0e229f4a25465996cd77
quanjiang.yue <yuequanjiang@avalongames.cn>
2022-10-11 22:20:53 +0800

    fix

--- packer_source.sh
+++ packer_source.sh
@@ -9,38 +9,30 @@ cd ${WORKSPACE}/and_channel
 for var in ${arr[@]}
 do
   # echo $var
-  version=$(${\$var##*-})
-  name=$(${\$var%*-})
-  echo $version
-  echo $name
+  echo ${\$var##*-}
+  echo ${\$var%*-}
 done
 
 arr=(`echo ${CD_AND_PLUGINS} | tr ',' ' '` )
 cd ${WORKSPACE}/and_plugin
 for var in ${arr[@]}
 do
-  version=$(${\$var##*-})
-  name=$(${\$var%*-})
-  echo $version
-  echo $name
+  echo ${\$var##*-}
+  echo ${\$var%*-}
 done
 
 arr=(`echo ${CD_IOS_CHANNELS} | tr ',' ' '` )
 cd ${WORKSPACE}/ios_channel
 for var in ${arr[@]}
 do
-  version=$(${\$var##*-})
-  name=$(${\$var%*-})
-  echo $version
-  echo $name
+  echo ${\$var##*-}
+  echo ${\$var%*-}
 done
 
 arr=(`echo ${CD_IOS_PLUGINS} | tr ',' ' '` )
 cd ${WORKSPACE}/ios_plugin
 for var in ${arr[@]}
 do
-  version=$(${\$var##*-})
-  name=$(${\$var%*-})
-  echo $version
-  echo $name
+  echo ${\$var##*-}
+  echo ${\$var%*-}
 done
