3ef432077adbccced3d8dd855f82143c801d9310
quanjiang.yue <yuequanjiang@avalongames.cn>
2022-10-11 22:12:56 +0800

    fix

--- packer_source.sh
+++ packer_source.sh
@@ -5,29 +5,42 @@ ftpUser=webuser
 ftpPassword=vy6Ks348a7s88
 
 arr=(`echo ${CD_AND_CHANNELS} | tr ',' ' '` )
-
+cd ${WORKSPACE}/and_channel
 for var in ${arr[@]}
 do
-  echo $var
+  # echo $var
+  version=${$var##*-}
+  name=${var%*-}
+  echo $version
+  echo $name
 done
 
 arr=(`echo ${CD_AND_PLUGINS} | tr ',' ' '` )
-
+cd ${WORKSPACE}/and_plugin
 for var in ${arr[@]}
 do
-  echo $var
+  version=${$var##*-}
+  name=${var%*-}
+  echo $version
+  echo $name
 done
 
 arr=(`echo ${CD_IOS_CHANNELS} | tr ',' ' '` )
-
+cd ${WORKSPACE}/ios_channel
 for var in ${arr[@]}
 do
-  echo $var
+  version=${$var##*-}
+  name=${var%*-}
+  echo $version
+  echo $name
 done
 
 arr=(`echo ${CD_IOS_PLUGINS} | tr ',' ' '` )
-
+cd ${WORKSPACE}/ios_plugin
 for var in ${arr[@]}
 do
-  echo $var
+  version=${$var##*-}
+  name=${var%*-}
+  echo $version
+  echo $name
 done
